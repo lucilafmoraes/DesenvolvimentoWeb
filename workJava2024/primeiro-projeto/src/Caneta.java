1c924f5c09406fb3c9ddb86e34545dfd000fc863
Lucila <105459895+lucilafmoraes@users.noreply.github.com>
2024-08-06 17:17:23 -0300

    add

---
+++ workJava2024/primeiro-projeto/src/Caneta.java
@@ -0,0 +1,6 @@
+public class Caneta {
+    String cor;
+    String marca;
+    double tamanho;
+    boolean tampa;
+}
---
+++ workJava2024/primeiro-projeto/src/TestaCanetas.java
@@ -0,0 +1,15 @@
+public class TestaCanetas {
+     public static void main(String[] args){
+        System.out.println("Testando construção de canetas...");
+
+        Caneta umaCaneta = new Caneta(); //ou null
+
+        umaCaneta.cor="Azul";
+        umaCaneta.marca="bic";
+        umaCaneta.tamanho= 12;
+
+        System.out.println("umaCaneta tem a cor "+umaCaneta.cor+" e tem a marca "+umaCaneta.marca);
+        System.out.println("Tamanho: "+umaCaneta. );
+
+     }
+}
